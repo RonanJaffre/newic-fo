6a371b69e5ef73af4a088e8a6aed8c0a72d4990f
Ronan Jaffré <ronan.jaffre@gmail.com>
2025-11-28 00:38:28 +0300

    home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,53 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4321', 
+  site: "http://localhost:4321",
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@': '/src/',
+        "@": "/src/",
       },
     },
     server: {
       watch: {
-        usePolling: true, 
+        usePolling: true,
       },
     },
   },
-  integrations: [sitemap({
-    // Génère automatiquement le sitemap.xml
-    filter: (page) => !page.includes('/admin/'),
-  }), react()],
+  integrations: [
+    sitemap({
+      // Génère automatiquement le sitemap.xml
+      filter: (page) => !page.includes("/admin/"),
+    }),
+    react(),
+  ],
   image: {
     // Permet d'optimiser les images depuis WordPress
-    domains: ['starterkit-blog.local'], 
-    remotePatterns: [{
-      protocol: 'http',
-      hostname: 'starterkit-blog.local',
-    }],
+    domains: ["starterkit-blog.local"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "starterkit-blog.local",
+      },
+    ],
   },
-  output: 'static', // Génération statique pour performance maximale
+  output: "static", // Génération statique pour performance maximale
   build: {
-    inlineStylesheets: 'auto',
+    inlineStylesheets: "auto",
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Figtree",
+        cssVariable: "--font-figtree",
+      },
+    ],
+    svgo: true,
   },
 });
---
+++ src/config/env.ts
@@ -0,0 +1,5 @@
+export const env = {
+  SITE_NAME: import.meta.env.PUBLIC_SITE_NAME,
+  BASE_URL: import.meta.env.PUBLIC_WP_API_URL,
+  SITE_URL: import.meta.env.PUBLIC_SITE_URL,
+};
