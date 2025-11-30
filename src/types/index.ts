e1f778fc648cbfed6250389640d66cbc283debda
Ronan Jaffré <ronan.jaffre@gmail.com>
2025-11-30 07:47:14 +0300

    news & new detail

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface Article {
+  id: string;
+  category: string;
+  date: string;
+  readTime: string;
+  title: string;
+  excerpt: string;
+  image: string;
+  link: string;
+  badge?: string;
+  content?: string;
+  tags?: string[];
+  cover?: string;
+  tagDescription?: string;
+}
