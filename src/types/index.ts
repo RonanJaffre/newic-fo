b88543bbebfbbc5c9a40d921e72072c30536e49c
Ronan Jaffré <ronan.jaffre@gmail.com>
2025-12-04 06:34:34 +0300

    fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,42 @@
 export interface Article {
   id: string;
+  slug: string;
   category: string;
   date: string;
-  readTime: string;
-  title: string;
-  excerpt: string;
+  readingTime: string;
+  title?:
+    | {
+        rendered?: string;
+      }
+    | string;
+  excerpt:
+    | {
+        rendered?: string;
+      }
+    | string;
   image: string;
   link: string;
   badge?: string;
-  content?: string;
+  content?:
+    | {
+        rendered?: string;
+      }
+    | string;
   tags?: string[];
   summary?: string[];
   cover?: string;
   tagDescription?: string;
+  _embedded?: Record<
+    string,
+    {
+      source_url?: string;
+      media_details?: {
+        width?: number;
+        height?: number;
+      };
+    }[]
+  >;
+  featuredImage: string;
+  imageWidth?: number;
+  imageHeight?: number;
 }
